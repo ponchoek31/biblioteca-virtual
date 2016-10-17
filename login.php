db0635e9fb39660263e755440f79a839da19c4ff
ponchoek31 <a.prospero31@gmail.com>
2016-10-16 23:27:50 -0600

    Add files via upload

--- login.php
+++ login.php
@@ -1,1 +1,13 @@
-?<?php  ?>
+<?php 
+function conectar(){
+	$user="root";
+	$pass="";
+	$server="localhost";
+	$db="biblio"
+	$con=mysql_connect($server,$user,$pass) or die("error al conectar, intentalo nuevamente".mysql_error());
+	mysql_select_db($db,$con);
+
+	return $con;
+} 
+
+ ?>
